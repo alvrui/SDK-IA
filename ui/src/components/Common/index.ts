630c8d6d1c9b3082675df6505ed5ff4eb5b8b320
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-08 11:17:18 +0200

    Add Common components index

---
+++ ui/src/components/Common/index.ts
@@ -0,0 +1,8 @@
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export { default as Sidebar } from './Sidebar';
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Modal } from './Modal';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as ErrorDisplay } from './ErrorDisplay';
