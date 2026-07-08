510a4203d94284352b7d76a16055f7aa7944c409
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-08 11:22:58 +0200

    Add Generation components index

---
+++ ui/src/components/Generation/index.ts
@@ -0,0 +1,3 @@
+export { default as GenerationInterface } from './GenerationInterface';
+export { default as GenerationForm } from './GenerationForm';
+export { default as GenerationResults } from './GenerationResults';
