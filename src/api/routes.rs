8ddac169860500be0ecab7a9314386de1bd22691
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-09 00:56:30 +0200

    Add configure function wrapper for main.rs integration

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -5,3 +5,7 @@ use crate::api::handlers::hollywood_animal::configure_hollywood_animal_routes;
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     configure_hollywood_animal_routes(cfg);
 }
+
+pub fn configure() -> impl actix_web::dev::Service<actix_http::Request, Response = actix_web::dev::ServiceResponse, Error = actix_web::Error> {
+    web::scope("/api/v1/internal").configure(configure_routes)
+}
