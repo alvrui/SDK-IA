5ff15818c3216d130df79c59a2890db2d1f4216e
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-09 04:37:01 +0200

    Fix domain/mod.rs: add agent module export

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -4,9 +4,11 @@ pub mod narrative;
 pub mod story_element;
 pub mod event;
 pub mod hollywood_animal;
+pub mod agent;
 
 pub use project::*;
 pub use narrative::*;
 pub use story_element::*;
 pub use event::*;
 pub use hollywood_animal::*;
+pub use agent::*;
