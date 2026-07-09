ba8b88d250fcce9381cf1831fd8afbdb5485c07f
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-09 15:11:02 +0200

    Fix: Correct configure function signature

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -8,6 +8,6 @@ pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     configure_project_routes(cfg);
 }
 
-pub fn configure() -> impl actix_web::dev::Service<actix_web::dev::ServiceRequest, Response = actix_web::dev::ServiceResponse, Error = actix_web::Error> {
-    web::scope("/api/v1/internal").configure(configure_routes)
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/api/v1/internal").configure(configure_routes));
 }
