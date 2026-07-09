52626f6fc4df17dd92cbea144c59fb535cd9ab92
alvrui <61255408+alvrui@users.noreply.github.com>
2026-07-09 04:53:27 +0200

    Fix web/mod.rs: use actix_files properly with ServiceConfig

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,7 +1,10 @@
 // Web module for serving static files
 
 use actix_web::web;
+use actix_files as fs;
 
-pub fn configure() -> impl actix_web::dev::Service<actix_web::dev::ServiceRequest, Response = actix_web::dev::ServiceResponse, Error = actix_web::Error> {
-    actix_files::Files::new("./ui/dist", "./").index_file("index.html")
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        fs::Files::new("./ui/dist", "./").index_file("index.html")
+    );
 }
